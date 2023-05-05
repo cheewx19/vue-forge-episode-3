cfd2ba8087eb96d141120be24633ffbb12f9bd67
cheewx19 <cheewx@hotmail.com>
2023-05-06 01:51:44 +0800

    update

--- components/ChatBox.vue
+++ components/ChatBox.vue
@@ -1,3 +1,116 @@
+<script lang="ts" setup>
+import { nanoid } from "nanoid";
+import { Message, User } from "~~/types";
+
+const props = defineProps<{
+  messages: Message[];
+  users: User[];
+  me: User;
+  usersTyping?: User[];
+}>();
+
+const usersTypingText = computed(() => {
+  if (!props.usersTyping?.length) return "";
+
+  if (props.usersTyping.length === 1) {
+    return `${props.usersTyping[0].name} is typing...`;
+  }
+
+  if (props.usersTyping.length > 3) {
+    return `${props.usersTyping.length} people are typing...`;
+  }
+});
+
+const emit = defineEmits<{
+  (event: "newMessage", newMessage: Message): void;
+}>();
+
+function getUser(userId: string) {
+  return props.users.find((user) => user.id === userId);
+}
+
+const messageBox = ref<HTMLElement>();
+watch(
+  () => props.messages.length,
+  async () => {
+    await nextTick();
+    if (messageBox.value) {
+      messageBox.value.scrollTop = messageBox.value.scrollHeight;
+    }
+  }
+);
+
+const isOpen = ref(true);
+const textMessage = ref("");
+function sendMessage() {
+  emit("newMessage", {
+    id: nanoid(),
+    userId: props.me.id,
+    createdAt: new Date(),
+    text: textMessage.value,
+  });
+  textMessage.value = "";
+}
+</script>
+
 <template>
-  <!-- Code out the chatbox for Exercise 1 here -->
+  <div class="fixed bottom-[10px] right-[10px]">
+    <button
+      v-show="!isOpen"
+      class="bg-blue-500 p-3 rounded"
+      @click="isOpen = true"
+    >
+      <IconChat class="w-8 h-8 text-white" />
+    </button>
+    <div
+      v-if="isOpen"
+      class="box bg-gray-300 dark:bg-gray-800 rounded w-[450px]"
+    >
+      <header
+        class="bg-gray-200 dark:bg-gray-900 px-4 flex justify-between items-center"
+      >
+        Customer Support Chat
+        <button class="p-4 pr-0" @click="isOpen = false">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </button>
+      </header>
+      <div ref="messageBox" class="messages p-4 overflow-y-scroll max-h-[80vh]">
+        <ChatBubble
+          v-for="message in messages"
+          :key="message.id"
+          :user="getUser(message.userId)"
+          :message="message"
+          :is-mine="me.id === message.userId"
+        />
+        <ChatBubble v-for="user in usersTyping" :user="user">
+          <AppLoading />
+        </ChatBubble>
+      </div>
+      <footer class="p-2">
+        <div v-if="usersTyping?.length" class="h-6 px-2 text-sm italic">
+          {{ usersTypingText }}
+        </div>
+        <input
+          type="text"
+          v-model="textMessage"
+          @keypress.enter.exact.prevent="sendMessage"
+          placeholder="Type your message"
+          class="input w-full block"
+        />
+      </footer>
+    </div>
+  </div>
 </template>
---
+++ components/ChatBubble.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import { Message, User } from "~~/types";
+// @ts-expect-error: no types
+import Markdown from "vue3-markdown-it";
+const props = defineProps<{
+  user?: User;
+  message?: Message;
+  isMine?: boolean;
+}>();
+
+const relativeTime = useTimeAgo(() => props.message?.createdAt ?? new Date());
+</script>
+
+<template>
+  <div class="chat" :class="{ 'chat-end': isMine, 'chat-start': !isMine }">
+    <div class="chat-image avatar">
+      <div class="w-10 rounded-full">
+        <img :src="user?.avatar" />
+      </div>
+    </div>
+    <div class="chat-header text-xs opacity-50 mb-2">
+      <strong>
+        {{ user?.name }}
+      </strong>
+      &nbsp;
+      <time v-if="message">
+        {{ relativeTime }}
+      </time>
+    </div>
+    <div class="chat-bubble dark:bg-gray-900 max-w-max w-full prose-sm py-0">
+      <Markdown v-if="message" :source="message.text" class="w-full" />
+      <slot v-else></slot>
+    </div>
+  </div>
+</template>
